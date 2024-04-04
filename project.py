7de57067e84f709a484f7a21ad9c81bd117aa657
Madhura1410 <madhugawande148@gmail.com>
2024-04-04 22:14:34 +0530

    New code added

--- project.py
+++ project.py
@@ -1,110 +1,52 @@
-import tkinter as tk
-import random
+import turtle as t
+def rectangle(horizontal, vertical, color):
+    t.pendown()
+    t.pensize(1)
+    t.color(color)
+    t.begin_fill()
+    for counter in range(1, 3):
+        t.forward(horizontal)
+        t.right(90)
+        t.forward(vertical)
+        t.right(90)
+    t.end_fill()
+    t.penup()
 
-class Game2048:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("2048")
-        self.master.geometry("400x400")
-        self.master.bind("<Key>", self.handle_key)
-
-        self.grid_size = 4
-        self.grid = [[0] * self.grid_size for _ in range(self.grid_size)]
-        self.score = 0
-
-        self.init_grid()
-        self.add_tile()
-        self.update_grid()
-
-    def init_grid(self):
-        self.tiles = []
-        for i in range(self.grid_size):
-            row = []
-            for j in range(self.grid_size):
-                tile = tk.Label(self.master, text="", font=("Helvetica", 32), width=4, height=2, relief="raised")
-                tile.grid(row=i, column=j, padx=5, pady=5)
-                row.append(tile)
-            self.tiles.append(row)
-
-    def add_tile(self):
-        empty_cells = [(i, j) for i in range(self.grid_size) for j in range(self.grid_size) if self.grid[i][j] == 0]
-        if empty_cells:
-            i, j = random.choice(empty_cells)
-            self.grid[i][j] = 2 if random.random() < 0.9 else 4
-
-    def update_grid(self):
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                value = self.grid[i][j]
-                if value == 0:
-                    self.tiles[i][j].configure(text="", bg="lightgray")
-                else:
-                    self.tiles[i][j].configure(text=str(value), bg="lightblue")
-        self.master.update_idletasks()
-
-    def handle_key(self, event):
-        if event.keysym in ['Up', 'Down', 'Left', 'Right']:
-            self.move_tiles(event.keysym)
-            self.add_tile()
-            self.update_grid()
-            if self.check_game_over():
-                print("Game Over! Score:", self.score)
-
-    def move_tiles(self, direction):
-        if direction == 'Up':
-            self.grid = self.transpose(self.grid)
-            self.grid = self.merge_tiles(self.grid)
-            self.grid = self.transpose(self.grid)
-        elif direction == 'Down':
-            self.grid = self.reverse(self.transpose(self.grid))
-            self.grid = self.merge_tiles(self.grid)
-            self.grid = self.transpose(self.reverse(self.grid))
-        elif direction == 'Left':
-            self.grid = self.merge_tiles(self.grid)
-        elif direction == 'Right':
-            self.grid = self.reverse(self.grid)
-            self.grid = self.merge_tiles(self.grid)
-            self.grid = self.reverse(self.grid)
-
-    def merge_tiles(self, grid):
-        score = 0
-        for i in range(self.grid_size):
-            j = 0
-            while j < self.grid_size - 1:
-                if grid[i][j] == grid[i][j+1] and grid[i][j] != 0:
-                    grid[i][j] *= 2
-                    score += grid[i][j]
-                    grid[i][j+1] = 0
-                    j += 2
-                else:
-                    j += 1
-        self.score += score
-        return grid
-
-    def check_game_over(self):
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                if self.grid[i][j] == 0:
-                    return False
-                if j < self.grid_size - 1 and self.grid[i][j] == self.grid[i][j+1]:
-                    return False
-                if i < self.grid_size - 1 and self.grid[i][j] == self.grid[i+1][j]:
-                    return False
-        return True
-
-    @staticmethod
-    def transpose(matrix):
-        return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
-
-    @staticmethod
-    def reverse(matrix):
-        return [row[::-1] for row in matrix]
-
-def main():
-    root = tk.Tk()
-    game = Game2048(root)
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
-
+t.penup()
+t.speed('slow')
+t.bgcolor('Dodger blue')
+# feet
+t.goto(-100, -150)
+rectangle(50, 20, 'blue')
+t.goto(-30, -150)
+rectangle(50, 20, 'blue')
+# legs
+t.goto(-25, -50)
+rectangle(15, 100, 'grey')
+t.goto(-55, -50)
+rectangle(-15, 100, 'grey')
+# body
+t.goto(-90, 100)
+rectangle(100, 150, 'red')
+# arms
+t.goto(-150, 70)
+rectangle(60, 15, 'grey')
+t.goto(-150, 110)
+rectangle(15, 40, 'grey')
+t.goto(10, 70)
+rectangle(60, 15, 'grey')
+t.goto(55, 110)
+rectangle(15, 40, 'grey')
+# neck
+t.goto(-50, 120)
+rectangle(15, 20, 'grey')
+# head
+t.goto(-85, 170)
+rectangle(80, 50, 'red')
+# eyes
+t.goto(-60, 160)
+rectangle(30, 10, 'white')
+t.goto(-55, 155)
+rectangle(5, 5, 'black')
+t.goto(-40, 155)
+rectangle(5, 5, 'black')

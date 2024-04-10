4a62904b16a98f76d85f414bb84a128624f39d76
Madhura1410 <madhugawande148@gmail.com>
2024-04-10 17:59:04 +0530

    modified file

--- project.py
+++ project.py
@@ -1,52 +1,84 @@
-import turtle as t
-def rectangle(horizontal, vertical, color):
-    t.pendown()
-    t.pensize(1)
-    t.color(color)
-    t.begin_fill()
-    for counter in range(1, 3):
-        t.forward(horizontal)
-        t.right(90)
-        t.forward(vertical)
-        t.right(90)
-    t.end_fill()
-    t.penup()
+# Python Program to illustrate 
+# Hangman Game 
+import random 
+from collections import Counter 
 
-t.penup()
-t.speed('slow')
-t.bgcolor('Dodger blue')
-# feet
-t.goto(-100, -150)
-rectangle(50, 20, 'blue')
-t.goto(-30, -150)
-rectangle(50, 20, 'blue')
-# legs
-t.goto(-25, -50)
-rectangle(15, 100, 'grey')
-t.goto(-55, -50)
-rectangle(-15, 100, 'grey')
-# body
-t.goto(-90, 100)
-rectangle(100, 150, 'red')
-# arms
-t.goto(-150, 70)
-rectangle(60, 15, 'grey')
-t.goto(-150, 110)
-rectangle(15, 40, 'grey')
-t.goto(10, 70)
-rectangle(60, 15, 'grey')
-t.goto(55, 110)
-rectangle(15, 40, 'grey')
-# neck
-t.goto(-50, 120)
-rectangle(15, 20, 'grey')
-# head
-t.goto(-85, 170)
-rectangle(80, 50, 'red')
-# eyes
-t.goto(-60, 160)
-rectangle(30, 10, 'white')
-t.goto(-55, 155)
-rectangle(5, 5, 'black')
-t.goto(-40, 155)
-rectangle(5, 5, 'black')
+someWords = '''apple banana mango strawberry 
+orange grape pineapple apricot lemon coconut watermelon 
+cherry papaya berry peach lychee muskmelon'''
+
+someWords = someWords.split(' ') 
+# randomly choose a secret word from our "someWords" LIST. 
+word = random.choice(someWords) 
+
+if __name__ == '__main__': 
+	print('Guess the word! HINT: word is a name of a fruit') 
+
+	for i in word: 
+		# For printing the empty spaces for letters of the word 
+		print('_', end=' ') 
+	print() 
+
+	playing = True
+	# list for storing the letters guessed by the player 
+	letterGuessed = '' 
+	chances = len(word) + 2
+	correct = 0
+	flag = 0
+	try: 
+		while (chances != 0) and flag == 0: # flag is updated when the word is correctly guessed 
+			print() 
+			chances -= 1
+
+			try: 
+				guess = str(input('Enter a letter to guess: ')) 
+			except: 
+				print('Enter only a letter!') 
+				continue
+
+			# Validation of the guess 
+			if not guess.isalpha(): 
+				print('Enter only a LETTER') 
+				continue
+			elif len(guess) & gt > 1: 
+				print('Enter only a SINGLE letter') 
+				continue
+			elif guess in letterGuessed: 
+				print('You have already guessed that letter') 
+				continue
+
+			# If letter is guessed correctly 
+			if guess in word: 
+				# k stores the number of times the guessed letter occurs in the word 
+				k = word.count(guess) 
+				for _ in range(k): 
+					letterGuessed += guess # The guess letter is added as many times as it occurs 
+
+			# Print the word 
+			for char in word: 
+				if char in letterGuessed and (Counter(letterGuessed) != Counter(word)): 
+					print(char, end=' ') 
+					correct += 1
+				# If user has guessed all the letters 
+				# Once the correct word is guessed fully, 
+				elif (Counter(letterGuessed) == Counter(word)): 
+																# the game ends, even if chances remain 
+					print("The word is: ", end=' ') 
+					print(word) 
+					flag = 1
+					print('Congratulations, You won!') 
+					break # To break out of the for loop 
+					break # To break out of the while loop 
+				else: 
+					print('_', end=' ') 
+
+		# If user has used all of his chances 
+		if chances & lt<= 0 and (Counter(letterGuessed) != Counter(word)): 
+			print() 
+			print('You lost! Try again..') 
+			print('The word was {}'.format(word)) 
+
+	except KeyboardInterrupt: 
+		print() 
+		print('Bye! Try again.') 
+		exit() 
